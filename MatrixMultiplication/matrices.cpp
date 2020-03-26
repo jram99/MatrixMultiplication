d4a60ccd06b5f54446eb00a6eca8d70f9005ed37
jram99 <ramseyerje17@gcc.edu>
2020-03-25 21:36:33 -0400

    More variables

--- MatrixMultiplication/matrices.cpp
+++ MatrixMultiplication/matrices.cpp
@@ -1,5 +1,6 @@
 // C program to multiply two rectangular matrices 
 #include<stdio.h> 
+using namespace std;
   
 // Multiplies two matrices mat1[][] and mat2[][] 
 // and prints result. 
@@ -8,25 +9,25 @@
 void multiply(int row1, int col1, int mat1[][col1], 
             int row2, int col2, int mat2[][col2]) 
 { 
-    int x, i, j; 
+    int x, r, c; 
     int res[row1][col2];
-    for (i = 0; i < row1; i++) 
+    for (r = 0; r < row1; r++) 
     { 
-        for (j = 0; j < col2; j++) 
+        for (c = 0; c < col2; c++) 
         { 
-            res[i][j] = 0; 
+            res[r][c] = 0; 
             for (x = 0; x < col1; x++) 
             { 
-                *(*(res + i) + j) += *(*(mat1 + i) + x) * 
-                                    *(*(mat2 + x) + j); 
+                *(*(res + r) + c) += *(*(mat1 + r) + x) * 
+                                    *(*(mat2 + x) + c); 
             } 
         } 
     } 
-    for (i = 0; i < row1; i++) 
+    for (r = 0; r < row1; r++) 
     { 
-        for (j = 0; j < col2; j++) 
+        for (c = 0; c < col2; c++) 
         { 
-            printf("%d ", *(*(res + i) + j)); 
+            printf("%d ", *(*(res + r) + c)); 
         } 
         printf("\n"); 
     } 
@@ -35,7 +36,9 @@ void multiply(int row1, int col1, int mat1[][col1],
 // Driver code 
 int main() 
 { 
-    int mat1[][2] = { { 2, 4 }, { 3, 4 } }; 
+    int const val = 2;
+    int val2 = 2;
+    int mat1[][val] = { { 2, 4 }, { 3, 4 } }; 
     int mat2[][2] = { { 1, 2 }, { 1, 3 } }; 
     int row1 = 2, col1 = 2, row2 = 2, col2 = 2; 
     multiply(row1, col1, mat1, row2, col2, mat2); 
